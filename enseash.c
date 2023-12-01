a4887a29e469750c540e5bafb94bc979226646e2
Nouhaila Faris <146836767+NouhailaFaris@users.noreply.github.com>
2023-12-01 17:40:12 +0100

    question 2 tp 1
    Gestion de la sortie du shell avec la commande “exit”

--- enseash.c
+++ enseash.c
@@ -1,10 +1,10 @@
 #include <unistd.h>
 #include <string.h>
+#include <stdlib.h>
 #include <sys/wait.h>
-
 #define WELCOME_MSG "Bienvenue dans le Shell ENSEA.\nPour quitter, tapez 'exit'.\n"
 #define PROMPT "enseash%"
-
+#define EXIT "Bye bye..."
 int main() {
     /* Buffer for storing the user's command */
     char command[100];
@@ -18,21 +18,34 @@ int main() {
         write(1, PROMPT, strlen(PROMPT));
 
         /* Read the command from the user */
-        int bytes_read = read(0, command, sizeof(command));
+        int bytes_read = read(0, command, 100);
+        command[bytes_read-1] = 0;
 
-        // Fork to create a child process
-        pid_t pid = fork();
-        if (pid == 0) {
+        /* Check if the command is "exit" */
+        if (strcmp(command, "exit") == 0) {
+            /* Exit the shell */
+            write(1, EXIT, strlen(EXIT));
+            exit(0);
+
+        }
+        else {
+            // Fork to create a child process
+            pid_t pid = fork();
+
             // Child process
-            command[bytes_read-1] = 0;  // Remove the newline character
-            execlp(command, command, (char *) NULL);
-        } else {
-            // Parent process
-            // Wait for the child process to finish
-            wait(NULL);
+            if (pid == 0) {
+                execlp(command, command, (char *) NULL);
+                /* Exit with a failure status if exec fails */
+                exit(1);
+
+            }
+                // Parent process
+                // Wait for the child process to finish
+            else {
+                wait(NULL);
+            }
         }
     }
-
     return 0;
 }
 
